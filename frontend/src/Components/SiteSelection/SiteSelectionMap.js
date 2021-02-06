f392da929860055676f9c2c8baa7ced77bfd3eb5
Max Spijkerbosch <spijkemax@ecs.vuw.ac.nz>
2021-02-06 16:22:45 +1300

    Update satellite map for full screen functionality and scale, and remove the option for regular map view.

--- frontend/src/Components/SiteSelection/SiteSelectionMap.js
+++ frontend/src/Components/SiteSelection/SiteSelectionMap.js
@@ -1,36 +1,14 @@
-import React, { createRef, Component } from "react";
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-// import { useScreenshot } from 'use-react-screenshot';
+import React, { useState } from "react";
 /* global google */
 let markerArray = [];
 
-
- 
-   const SiteSelectionMap = (props) => {
-    const ref = createRef(null)
-//   const [image, takeScreenshot] = useScreenshot()
-//   const getImage = () => takeScreenshot(ref.current)
-//       //'Continue' event handler, calls the nextStep method from MultiStepHandler to go forward a page
-//       const continuee = e => {
-//         e.preventDefault();
-//         this.props.nextStep();
-//     }
-    //'back' event handler, calls the prevStep method from MultiStepHandler to go back a page
-     const back = e => {
-        e.preventDefault();
-        this.props.prevStep();
-      }
+   const SiteSelectionMap = () => {
         return (
-          <div>  
+          <div>   
           <div>
+            <h2>Dont have a site plan? Save a screenshot of one on our satellite map! Press Windows Key(⊞) and Print screen (prt sc) to take the shot.</h2>
         </div>
-        {/* <img width={600} src={image} alt={'Screenshot'} /> */}
-        <div ref={ref}>
-        <iframe src="https://snazzymaps.com/embed/289197" title = "map" width="100%" height="600px"></iframe> 
-        </div>
-        {/* <button style={{ marginBottom: '10px' }} onClick={getImage}>
-          Take screenshot
-        </button> */}
+        <iframe src="https://snazzymaps.com/embed/289197" title = "map" width="100%" height="600px" allow="fullscreen"></iframe> 
         </div>
         );
       }
