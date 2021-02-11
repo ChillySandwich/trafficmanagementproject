888195421170d8d31c7b17c881831dde4ef40d86
spijkemax <spijkemax@ecs.vuw.ac.nz>
2021-02-11 14:33:49 +1300

    Adding help buttons on Site Selection components

--- frontend/src/Components/SiteSelection/SiteSelectionMap.js
+++ frontend/src/Components/SiteSelection/SiteSelectionMap.js
@@ -1,11 +1,30 @@
 import React, { useState } from "react";
+import { Grid, Button } from '@material-ui/core';
+
 /* global google */
 let markerArray = [];
 
    const SiteSelectionMap = () => {
+    const [buttonTextMac, setButtonTextMac] = useState("For Mac"); //same as creating your state variable where "Next" is the default value for buttonText and setButtonText is the setter function for your state variable instead of setState
+    const [buttonTextWindows, setButtonTextWindows] = useState("For Windows");
+
+    const changeText = (text) => setButtonTextMac(text);
+    const changeTextWindows = (text) => setButtonTextWindows(text);
         return (
-          <div>           
-        <iframe src="https://snazzymaps.com/embed/289197" title = "map" width="90%" height="600px" allow="fullscreen"></iframe> 
+          <div> 
+             <br></br> 
+            <h2> Screenshot a Site Map</h2>  
+            <Grid container spacing={3} alignItems="center">
+            <Grid item xs={6}>
+        <button className="helpButtons" value="For Mac" onClick={() => setButtonTextWindows("Windows Key + Prt Sc")}>{buttonTextWindows}</button>
+        </Grid>
+                        <Grid item xs={6}>
+                        <button className="helpButtons" color="white" value="For Mac" onClick={() => setButtonTextMac("Shift + Command + 3")}>{buttonTextMac}</button>
+        </Grid>
+              </Grid>       
+        <iframe style={{padding: "30px", backgroundColor: "white", borderRadius: "10px", border: "none"}} src="https://snazzymaps.com/embed/289197" title = "map" width="90%" height="600px" allow="fullscreen"></iframe> 
+       <div>
+        </div>
         </div>
         );
       }
